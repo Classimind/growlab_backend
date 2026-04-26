9283d34f0a684dd2f6010fbd85fc95cb17ee8075
whoamisafal <computerstha12@gmail.com>
2026-04-26 22:47:52 +0545

    Fixed bug

--- app/api/v1/farm.py
+++ app/api/v1/farm.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException,status
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from app.models.farm import Lab
 from app.services.lab_service import LabService
 from app.db.clients import get_db
-
+from pymongo.errors import PyMongoError
 from app.core.dependencies import get_current_user
 from app.core.roles import FarmRole,Role
 from app.core.dependencies import require_roles
+from bson import ObjectId
 farm_router = APIRouter()
 
 
@@ -27,18 +28,33 @@ async def create_lab(
     return await service.create_lab(lab)
 
 
-@farm_router.get("/")
+farm_router.get("/")
 async def get_all_labs(
     service: LabService = Depends(get_lab_service),
-    user=Depends(get_current_user)):
-    user_id = str(user["user_id"])
-    query = {
-        "$or": [
-            {"created_by": user_id},
-            {"employees.user_id": user_id}
-        ]
-    }
-    return await service.get_all_labs(query)
+    user=Depends(get_current_user),
+):
+    try:
+        user_id = ObjectId(user["user_id"])
+
+        query = {
+            "$or": [
+                {"created_by": user_id},
+                {"employees.user_id": user_id},
+            ]
+        }
+        return await service.get_all_labs(query)
+
+    except PyMongoError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Database error",
+        )
+
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Something went wrong",
+        )
 
 
 @farm_router.get("/{lab_id}")
